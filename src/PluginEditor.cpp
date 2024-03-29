5ae8bb999e19f6adede0a04384b106e1ce8cd6c2
Luke Ruatta <laruatta@gmail.com>
2024-03-29 10:24:35 -0500

    fixed FFT path positioning to be bounded by the responseCurve window

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -330,6 +330,7 @@ void ResponseCurveComponent::paint (juce::Graphics& g)
         responseCurve.lineTo(responseArea.getX() + i, map(mags[i]));
     }
 
+    leftChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(),responseArea.getY()));
     g.setColour(Colours::aliceblue);
     g.strokePath(leftChannelFFTPath, PathStrokeType(1.f));
 
