53faf733600d7149be04373cec53dfd7afacd50b
Luke Ruatta <laruatta@gmail.com>
2024-05-22 10:34:50 -0500

    Adding GoogleTest suite

---
+++ test/src/SimpleEQTest.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include "PluginProcessor.h"
+
+namespace SimpleEQTest {
+    TEST(SimpleEQAudioProcessor, Foo) {
+        SimpleEQAudioProcessor processor{};
+        ASSERT_FALSE(true);
+    }
+}
